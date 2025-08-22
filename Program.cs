5268d5ea1ceebf78f8f1917f1f4ba6ed7d5f9f8e
Dimas Aparecido Rabelo de Souza <146005383+DimasRabelo@users.noreply.github.com>
2025-08-21 22:13:43 -0300

    Primeiro commit - adicionando projeto ExemploPoo

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using ExemploPoo.Models;
+
+ContaCorrente c1 = new ContaCorrente(123, 1000);
+
+c1.ExibirSaldo();
+c1.Sacar(500);
+c1.ExibirSaldo();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Pessoa p1 = new Pessoa();
+// p1.Nome = "Dimas";
+// p1.Idade = 38;
+
+// p1.Apresentar();
