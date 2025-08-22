595f574563b220db18d560ec7bf0b98f3a4e58a8
Dimas Aparecido Rabelo de Souza <146005383+DimasRabelo@users.noreply.github.com>
2025-08-22 08:19:49 -0300

    Update

--- Program.cs
+++ Program.cs
@@ -1,10 +1,60 @@
 ﻿using ExemploPoo.Models;
 
-ContaCorrente c1 = new ContaCorrente(123, 1000);
 
-c1.ExibirSaldo();
-c1.Sacar(500);
-c1.ExibirSaldo();
+
+
+Aluno a1 = new Aluno();
+a1.Nome = "João";
+a1.Idade = 20;
+a1.Nota = 10;
+a1.Apresentar();
+
+Professor p1 = new Professor();
+p1.Nome = "Maria";
+p1.Idade = 40;
+p1.Salario = 5000;
+p1.Apresentar();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ContaCorrente c1 = new ContaCorrente(123, 1000);
+
+// c1.ExibirSaldo();
+// c1.Sacar(500);
+// c1.ExibirSaldo();
 
 
 
